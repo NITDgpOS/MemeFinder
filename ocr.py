a8327e2f5af654951d1900a5bd4c6654b18296b5
Aniq55 <aniqrah@gmail.com>
2017-12-07 02:03:10 +0530

    Added blur

--- ocr.py
+++ ocr.py
@@ -15,20 +15,11 @@ args = vars(ap.parse_args())
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-gray = cv2.threshold(gray, 0, 255,
-	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
  
-# write the grayscale image to disk as a temporary file so we can apply OCR to it
+gray = cv2.threshold(gray, 0, 255,
+gray = cv2.medianBlur(gray, 3)
+ 
+# write the grayscale image to disk as a temporary file so we can
+# apply OCR to it
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
-
-# load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-print(text)
- 
-# show the output images
-cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
-cv2.waitKey(0)
