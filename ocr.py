e40711bab8379738cbbb3a2834835fc632cd997e
Aniq55 <aniqrah@gmail.com>
2017-12-07 02:05:47 +0530

    change

--- ocr.py
+++ ocr.py
@@ -5,20 +5,19 @@ from pytesseract import image_to_string
 import argparse
 import cv2
 import os
- 
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image to be OCR'd")
+ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
 args = vars(ap.parse_args())
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 
+
 gray = cv2.threshold(gray, 0, 255,
 gray = cv2.medianBlur(gray, 3)
- 
+
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
