0acbfef418527f361109db851083d42f260d7ed7
Aniq55 <aniqrah@gmail.com>
2017-12-07 02:08:35 +0530

    syntax error fixed

--- ocr.py
+++ ocr.py
@@ -15,10 +15,22 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-gray = cv2.threshold(gray, 0, 255,
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
 gray = cv2.medianBlur(gray, 3)
 
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
-filename = "{}.png".format(os.getpid())
+filename= "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
+
+# load the image as a PIL/Pillow image, apply OCR, and then delete
+# the temporary file
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+ 
+# show the output images
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
