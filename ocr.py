b95c982247cffb1b8ed1a5ef1c965cc04931b2b1
Aniq55 <aniqrah@gmail.com>
2017-12-11 23:52:04 +0530

    Handling exception in ocr

--- ocr.py
+++ ocr.py
@@ -19,37 +19,40 @@ fwrite= open('data2.txt', 'w')
 # load the example image and convert it to grayscale
 #image = cv2.imread(args["image"])
 def extractText(image_path):
-	image= cv2.imread(image_path)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-	gray = cv2.medianBlur(gray, 3)
+    try:
+        image= cv2.imread(image_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        gray = cv2.medianBlur(gray, 3)
 
-	# write the grayscale image to disk as a temporary file so we can
-	# apply OCR to it
-	filename= "{}.png".format(os.getpid())
-	cv2.imwrite(filename, gray)
+        # write the grayscale image to disk as a temporary file so we can
+        # apply OCR to it
+        filename= "{}.png".format(os.getpid())
+        cv2.imwrite(filename, gray)
 
-	# load the image as a PIL/Pillow image, apply OCR, and then delete
-	# the temporary file
-	text = pytesseract.image_to_string(Image.open(filename))
-	os.remove(filename)
-	text= text.strip().split()
-	chars_to_remove = ['.', '!', ':']
-	extracted=list()
-	for t in text:
-		for c in chars_to_remove:
-			t=string.replace(t, c,'')
-		extracted.append(spell(t))
- 	return(' '.join(extracted))
+        # load the image as a PIL/Pillow image, apply OCR, and then delete
+        # the temporary file
+        text = pytesseract.image_to_string(Image.open(filename))
+        os.remove(filename)
+        text= text.strip().split()
+        chars_to_remove = ['.', '!', ':']
+        extracted=list()
+        for t in text:
+            for c in chars_to_remove:
+                t=string.replace(t, c,'')
+            extracted.append(spell(t))
+        return(' '.join(extracted))
 
-count = 0
-for line in fread:
-	if count>0:
-		image_location= line.split(',')[0]
-		fromImage= extractText(image_location)
-		fwrite.write(line[:-1]+","+ ''.join([i if ord(i) < 128 else ""  for i in fromImage])+ "\n")
-	else:
-		fwrite.write(line[:-1]+", ocr_out\n")
-	print(count)	
-	count= count+1
+    count = 0
+    for line in fread:
+        if count>0:
+            image_location= line.split(',')[0]
+            fromImage= extractText(image_location)
+            fwrite.write(line[:-1]+","+ ''.join([i if ord(i) < 128 else ""  for i in fromImage])+ "\n")
+        else:
+            fwrite.write(line[:-1]+", ocr_out\n")
+        print(count)    
+        count= count+1
 
+    except:
+        print("image skipped")
